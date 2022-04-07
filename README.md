e2b8c180873d5c7e30fa2705d7057406c5a9fda9
cypox <am_neggaz@esi.dz>
2022-04-07 07:17:54 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Maxwell's Demon game
+
+Allows you to play as Maxwell's demon and try to restore order to a system!
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,18 @@
 int main(int argc, char** argv)
 {
   std::cout << "maxwell demon game" << std::endl;
-    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-    // run the program as long as the window is open
-    while (window.isOpen())
+  sf::Window window(sf::VideoMode(800, 600), "Maxwell's Demon Game");
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
+  }
 
-    return 0;
   return 0;
 }
