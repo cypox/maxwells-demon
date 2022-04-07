d252ca819add69efe32cf90c017455236bb761a3
cypox <am_neggaz@esi.dz>
2022-04-07 07:09:21 +0200

    added sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
+#include <SFML/Window.hpp>
+
 
 int main(int argc, char** argv)
 {
   std::cout << "maxwell demon game" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
   return 0;
 }
