a7616e2b2376529aaeddd1b66b6d437451beb6a9
cypox <am_neggaz@esi.dz>
2022-04-09 19:39:05 +0200

    added gtest support

---
+++ test/include/system_test.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "system.h"
+
+
+TEST(TestSystem, IsRandom) 
+{
+  particle_system world(10, 0.0001, 1500, 900);
+  for (auto& p : world.get_particles())
+  {
+    EXPECT_TRUE(p._x <= 1500);
+    EXPECT_TRUE(p._x >= 0);
+    EXPECT_TRUE(p._y <= 900);
+    EXPECT_TRUE(p._y >= 0);
+  }
+}
---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+#include "system_test.h"
+
+
+int main(int argc, char **argv) 
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
